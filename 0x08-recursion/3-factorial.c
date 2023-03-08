5761315b62593db2ecb98703213ca184105b3896
Benjamin Adeyele <benadeyele@gmail.com>
2023-03-08 02:04:15 -0800

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * factorial - factorial of int
+ * 
+ * Return:int
+ */
+int factorial(int n)
+{
+	int y;
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		y = n * factorial(n - 1);
+	}
+			return (y);
+
+}
